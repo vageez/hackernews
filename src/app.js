1a3ad0d52a41054d3695d35327bc36748aceb011
Unknown <avagenas14@gmail.com>
2019-10-19 21:56:43 -0400

    Adding translations using react-intl

--- src/app.js
+++ src/app.js
@@ -2,9 +2,18 @@ import React from 'react'
 import ReactDOM from 'react-dom'
 import { Provider } from 'react-redux'
 import styled from 'styled-components'
+import { FormattedMessage, IntlProvider } from 'react-intl'
+import Cookie from 'js-cookie'
 import { store } from './store'
 import { media } from './media.query'
 import Stories from './components/Stories.jsx'
+import messages_fr from './translations/fr.json'
+import messages_en from './translations/en.json'
+
+const messages = {
+  fr: messages_fr,
+  en: messages_en
+}
 
 const AppWrapper = styled.div`
   font-family: Arial, Helvetica, sans-serif;
@@ -15,12 +24,23 @@ const H1 = styled.h1`
   font-size: 1.5em;
   ${media.MOBILE`{font-size:1.2em}`};
 `
+
+const locale = Cookie.get('locale') || 'en'
+
 const App = () => (
   <Provider store={store}>
-    <AppWrapper>
-      <H1>{`Hackernews Top 10 Stories!`}</H1>
-      <Stories />
-    </AppWrapper>
+    <IntlProvider locale={locale} messages={messages[locale]}>
+      <AppWrapper>
+        <H1>
+          <FormattedMessage
+            id={`app.hackerNewsH1`}
+            defaultMessage={`Hackernews Top 10 Stories!`}
+            description={`Hackernews H1 Title Tag`}
+          />
+        </H1>
+        <Stories />
+      </AppWrapper>
+    </IntlProvider>
   </Provider>
 )
 
